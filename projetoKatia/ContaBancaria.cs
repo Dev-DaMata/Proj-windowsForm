ec9afd4cd530105cc4fcd9f31e9d345eace46a32
GuilhermeCordeiroDaMata <gcordeiro773@gmail.com>
2023-10-25 13:09:16 -0300

    Iniciado o ambiente de desenvolvimento junto das classes

---
+++ projetoKatia/ContaBancaria.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projetoKatia
+{
+    public class ContaBancaria
+    {   
+        public string cliente { get; set; }
+        public int num_conta { get; set; }
+        public float saldo { get; set; }
+
+        public string depositar()
+        {
+
+        }
+    }
+
+    public class ContaPoupança : ContaBancaria
+    {
+        public int dia_rendimento { get; set; }
+
+        public string calcularNovoSaldo()
+        {
+
+        }   
+    }
+
+    public class ContaEspecial : ContaBancaria
+    {
+        public float limite { get; set; }
+
+        public string sacar()
+        {
+
+        }
+    }
+}
